a70f0a6d068818a23cfa554ff94e9668f06260a7
Fabio Janio <fabiojaniolima@gmail.com>
2021-09-27 01:20:59 -0300

    refactor: code visual adequacy

--- app/Http/Middleware/ResponseJson.php
+++ app/Http/Middleware/ResponseJson.php
@@ -3,17 +3,11 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class ResponseJson
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
 

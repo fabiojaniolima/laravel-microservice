f97cb26f501fa134c01462e258b9fca314da876c
Fabio Janio <fabiojaniolima@gmail.com>
2021-09-27 00:15:08 -0300

    chore: change ID from auto-incremental to UUIDv4

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasUuid;
 
     protected $fillable = [
         'name',
--- database/migrations/2021_09_26_114219_create_categories_table.php
+++ database/migrations/2021_09_26_114219_create_categories_table.php
@@ -9,7 +9,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->string('name');
             $table->boolean('is_active')->default(true);
             $table->text('description')->nullable();
